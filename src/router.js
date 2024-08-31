e779c29227be899888270231c2dc23f40db5ef43
Pedro Luiz Cabral Salomon Prado <pedroprado010@users.noreply.github.com>
2024-08-31 13:43:51 -0300

    cors and routes

--- src/router.js
+++ src/router.js
@@ -1,10 +1,33 @@
 const express = require('express')
+const helmet = require('helmet');
+const cors = require('cors')
 const app = express()
 const port = 3000
 
+app.use(helmet());
+app.use(cors())
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.json({ test: 123 })
 })
+
+app.post('/register', (req, res) => {
+  /**
+   * insert into postgres
+   */
+});
+
+app.post('/login', (req, res) => {
+  /**
+   * select from postgres
+   */
+});
+
+app.get('/cats', (req, res) => {
+  /**
+   * get from mongodb
+   */
+});
 
 module.exports.start = () => {
   app.listen(port, () => {
